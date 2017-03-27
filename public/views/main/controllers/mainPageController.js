ddb7a814738365dee66e89329ab7dc5261a394a9
Rohit Raul <raul.r@husky.neu.edu>
2017-03-27 17:34:22 -0400

    version 1.4 27/03 5:34

--- public/views/main/controllers/mainPageController.js
+++ public/views/main/controllers/mainPageController.js
@@ -1,8 +1,45 @@
 (function () {
     angular
         .module("BookHubMaker")
-        .controller("mainPageController",function () {
-           var vm=this;
-           vm.check="HELLO";
+        .controller("mainPageController", function () {
+            var vm = this;
+            vm.check = "HELLO";
+            vm.books = [
+                {
+                    name: "Book1 Loooooooooooooooooooooooooooooooooong",
+                    author: "Book1 Author",
+                    description: "adfadsfasdfasdfasfasdfasdfasddsfasd afasdfsdfasdfadfsfsfsafsdfasdf"
+                },
+                {
+                    name: "Book2 Loooooooooooooooooooooooooooooooooong",
+                    author: "Book2 Author",
+                    description: "adfadsfasdfasdfasfasdfasdfasddsfasd afasdfsdfasdfadfsfsfsafsdfasdf"
+                },
+                {
+                    name: "Book3 Loooooooooooooooooooooooooooooooooong",
+                    author: "Book3 Author",
+                    description: "adfadsfasdfasdfasfasdfasdfasddsfasd afasdfsdfasdfadfsfsfsafsdfasdf"
+                },
+                {
+                    name: "Book4 Loooooooooooooooooooooooooooooooooong",
+                    author: "Book4 Author",
+                    description: "adfadsfasdfasdfasfasdfasdfasddsfasd afasdfsdfasdfadfsfsfsafsdfasdf"
+                },
+                {
+                    name: "Book5 Loooooooooooooooooooooooooooooooooong",
+                    author: "Book5 Author",
+                    description: "adfadsfasdfasdfasfasdfasdfasddsfasd afasdfsdfasdfadfsfsfsafsdfasdf"
+                },
+                {
+                    name: "Book6 Loooooooooooooooooooooooooooooooooong",
+                    author: "Book6 Author",
+                    description: "adfadsfasdfasdfasfasdfasdfasddsfasd afasdfsdfasdfadfsfsfsafsdfasdf"
+                },
+                {
+                    name: "Book7 Loooooooooooooooooooooooooooooooooong",
+                    author: "Book7 Author",
+                    description: "adfadsfasdfasdfasfasdfasdfasddsfasd afasdfsdfasdfadfsfsfsafsdfasdf"
+                }
+            ];
         });
 })();
