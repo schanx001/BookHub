e55c177f41dd3fafb781beba63cb64dea42aa632
schanx001 <kawlekar.s@husky.neu.edu>
2017-03-27 16:23:38 -0400

    fixed bugs and added some code

---
+++ public/views/user/controllers/register.controller.client.js
@@ -0,0 +1,13 @@
+/**
+ * Created by schanx on 3/26/17.
+ */
+(function () {
+    angular
+        .module("BookHubMaker")
+        .controller("registerController", registerController);
+
+    function registerController($location) {
+        var vm = this;
+
+    }
+})();
