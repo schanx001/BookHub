2c7fc8a4ff331920aabd488d0c030a21d57afd6f
shohitb <shohitdoon@gmail.com>
2017-04-08 19:23:22 -0400

    email

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var nodemailer = require("nodemailer");
 var app = express();
 
 var bodyParser = require('body-parser');
@@ -9,6 +10,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 //require ("./test/app.js")(app);
+
+/*
+ Here we are configuring our SMTP Server details.
+ STMP is mail server which is responsible for sending and recieving email.
+ */
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: "webdevproject2017@gmail.com",
+        pass: "webdev123"
+    }
+});
+/*------------------SMTP Over-----------------------------*/
+
+/*------------------Routing Started ------------------------*/
+
+app.get('/',function(req,res){
+    res.sendfile('index.html');
+});
+app.get('/send',function(req,res){
+    var mailOptions={
+        to : req.query.to,
+        subject : req.query.subject,
+        text : req.query.text
+    };
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+            res.end("error");
+        }else{
+            console.log("Message sent: " + response.message);
+            res.end("sent");
+        }
+    });
+});
 
 require("./private/app.js")(app);
 var port = process.env.PORT || 3001;
